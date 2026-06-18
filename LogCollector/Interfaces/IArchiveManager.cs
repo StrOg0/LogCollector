da20afaa7cec41bd922c0782717343872a5d7227
RinR1n <le6edev2307@yandex.ru>
2026-06-18 16:13:56 +0300

    Add Interface for ArchiveManager

---
+++ LogCollector/Interfaces/IArchiveManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogCollector.Interfaces
+{
+    public interface IArchiveManager
+    {
+        List<string> ExtractArchivesRecursively(string sourceArchivePath, string targetDirectory);
+
+        string CreateResultArchive(string destinationArchivePath, Dictionary<string, string> logsByServer);
+    }
+}
