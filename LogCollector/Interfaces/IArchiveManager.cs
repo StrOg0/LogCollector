39172f03a737ffe90c8193d9e8c34cecc474572e
RinR1n <le6edev2307@yandex.ru>
2026-06-18 16:53:20 +0300

    Fix Interface for ArchiveManager

--- LogCollector/Interfaces/IArchiveManager.cs
+++ LogCollector/Interfaces/IArchiveManager.cs
@@ -1,9 +1,11 @@
-﻿namespace LogCollector.Interfaces
+﻿using LogCollector.Models;
+
+namespace LogCollector.Interfaces
 {
     public interface IArchiveManager
     {
-        List<string> ExtractArchives(string sourceArchivePath, string targetDirectory);
+        List<string> ExtractArchives(string sourceArchivePath, string targetDirectory, IProgress<ArchiveProgressInfo> progress = null);
 
-        string CreateResultArchive(string destinationArchivePath, Dictionary<string, string> logsByServer);
+        string CreateResultArchive(string destinationArchivePath, Dictionary<string, string> logsByServer, IProgress<ArchiveProgressInfo> progress = null);
     }
 }
