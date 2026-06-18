d64fafc71cd62a6862bb6adf2d66b3243e3e45de
RinR1n <le6edev2307@yandex.ru>
2026-06-18 16:48:01 +0300

    Add model for tracking ArchiveManager progress

---
+++ LogCollector/Models/ArchiveProgressInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogCollector.Models
+{
+    public class ArchiveProgressInfo
+    {
+        public string Stage { get; set; }
+
+        public string Message { get; set; }
+
+        public int Percent { get; set; } = -1;
+    }
+}
