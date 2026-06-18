8bcec3dbfedc5c70794345e25a762c1b379f1869
RinR1n <le6edev2307@yandex.ru>
2026-06-18 16:40:57 +0300

    Implement ExtractArchives method

---
+++ LogCollector/BLL/ArchiveManager.cs
@@ -0,0 +1,59 @@
+﻿using System.IO.Compression;
+using LogCollector.Interfaces;
+
+namespace LogCollector.BLL
+{
+    public class ArchiveManager : IArchiveManager
+    {
+        public List<string> ExtractArchives(string sourceArchivePath, string targetDirectory)
+        {
+            if (!File.Exists(sourceArchivePath))
+                throw new FileNotFoundException($"Архив не найден: {sourceArchivePath}");
+
+            Directory.CreateDirectory(targetDirectory);
+            var extractedLogFiles = new List<string>();
+            var archivesToProcess = new Queue<string>();
+
+            archivesToProcess.Enqueue(sourceArchivePath);
+
+            while (archivesToProcess.Count > 0)
+            {
+                string currentArchive = archivesToProcess.Dequeue();
+                string currentExtractDir = Path.Combine(targetDirectory, Path.GetFileNameWithoutExtension(currentArchive));
+
+                if (Directory.Exists(currentExtractDir))
+                    currentExtractDir += $"_{Guid.NewGuid():N}";
+
+                Directory.CreateDirectory(currentExtractDir);
+
+                try
+                {
+                    ZipFile.ExtractToDirectory(currentArchive, currentExtractDir);
+                }
+                catch (Exception ex)
+                {
+                    
+                    throw new InvalidDataException($"Ошибка распаковки архива {currentArchive}: {ex.Message}", ex);
+                }
+
+                foreach (var file in Directory.GetFiles(currentExtractDir, "*.*", SearchOption.AllDirectories))
+                {
+                    string ext = Path.GetExtension(file).ToLowerInvariant();
+
+                    if (ext == ".log")
+                    {
+                        extractedLogFiles.Add(file);
+                    }
+                    else if (ext == ".zip")
+                    {
+                        archivesToProcess.Enqueue(file);
+                    }
+                }
+            }
+
+            return extractedLogFiles;
+        }
+
+        
+    }
+}
--- LogCollector/Interfaces/IArchiveManager.cs
+++ LogCollector/Interfaces/IArchiveManager.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LogCollector.Interfaces
+﻿namespace LogCollector.Interfaces
 {
     public interface IArchiveManager
     {
-        List<string> ExtractArchivesRecursively(string sourceArchivePath, string targetDirectory);
+        List<string> ExtractArchives(string sourceArchivePath, string targetDirectory);
 
         string CreateResultArchive(string destinationArchivePath, Dictionary<string, string> logsByServer);
     }
